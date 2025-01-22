51cde8f3a6756f6ad3f934c976074a04bf5d7f58
professionalhomes <lednerdonny5@gmail.com>
2025-01-23 00:53:34 +0900

    feat: add-icon-comp

---
+++ src/components/common/Icon.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { Box, BoxProps } from "@chakra-ui/react";
+
+const Icon: FC<BoxProps> = (props) => {
+    return <Box {...props} />
+}
+
+export default Icon;
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,3 +3,4 @@ export { default as ModalCloseButton } from './ModalCloseButton';
 export { default as ModalContent } from './ModalContent';
 export { default as ModalOverlay } from './ModalOverlay';
 export { default as ModalTrigger } from './ModalTrigger';
+export { default as GuideModal } from './GuideModal';
