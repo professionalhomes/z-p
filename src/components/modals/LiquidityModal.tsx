884ee24bc5c74377a4a3e9605c807a05ee4cbd79
professionalhomes <lednerdonny5@gmail.com>
2025-01-23 21:44:27 +0900

    feat: add-some-modals

---
+++ src/components/modals/LiquidityModal.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+
+import { Heading, Text } from "@chakra-ui/react";
+
+import { Modal, ModalCloseButton, ModalContent, ModalOverlay } from "../common";
+import { ModalProps } from "../common/Modal";
+
+const LiquidityModal: FC<ModalProps> = (props) => {
+    return (
+        <Modal {...props}>
+            <ModalOverlay />
+            <ModalContent left={{ base: '50%', lg: '75%' }} p={8} w='full' maxW={{ base: '320px', lg: '420px' }} direction='column' gap={4}>
+                <ModalCloseButton />
+                <Heading as="h2" textAlign="center" size="lg">
+                    LIQUIDITY
+                </Heading>
+                <Text textAlign='center'>
+                    Coming soon...
+                </Text>
+            </ModalContent>
+        </Modal>
+    )
+}
+
+export default LiquidityModal;
