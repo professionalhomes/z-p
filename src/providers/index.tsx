f60cc1187e6347f25d452b983dd3b89ac5da547c
professionalhomes <lednerdonny5@gmail.com>
2025-01-23 21:46:12 +0900

    feat: add-some-modals-to-provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,9 +2,15 @@
 import { createContext, FC, ReactNode, useState } from "react";
 
 import AirdropModal from "@/components/modals/AirdropModal";
+import BridgeModal from "@/components/modals/BridgeModal";
 import { CubeGuideModal, ParticlesGuideModal, PasskeyGuideModal, ThemeGuideModal } from "@/components/modals/guides";
+import InfoModal from "@/components/modals/InfoModal";
+import LiquidityModal from "@/components/modals/LiquidityModal";
 import LoginModal from "@/components/modals/LoginModal";
+import ReceiveModal from "@/components/modals/ReceiveModal";
+import SendModal from "@/components/modals/SendModal";
 import StakingModal from "@/components/modals/StakingModal";
+import SwapModal from "@/components/modals/SwapModal";
 import { Provider as ThemeProvider } from "@/components/ui/provider";
 
 import SorobanReactProvider from "./SorobanReactProvider";
@@ -13,6 +19,12 @@ interface IApp {
     startAnimation?: boolean;
     setStartAnimation?: (startAnimation: boolean) => void;
     openLoginModal?: () => void;
+    openSendModal?: () => void;
+    openReceiveModal?: () => void;
+    openSwapModal?: () => void;
+    openLiquidityModal?: () => void;
+    openBridgeModal?: () => void;
+    openInfoModal?: () => void;
     openStakingModal?: () => void;
     openAirdropModal?: () => void;
     openParticlesGuideModal?: () => void;
@@ -29,6 +41,12 @@ const Provider: FC<Props> = ({ children }) => {
     const [showAirdropModal, setShowAirdropModal] = useState(false);
     const [showStakingModal, setShowStakingModal] = useState(false);
     const [showLoginModal, setShowLoginModal] = useState(false);
+    const [showSendModal, setShowSendModal] = useState(false);
+    const [showReceiveModal, setShowReceiveModal] = useState(false);
+    const [showSwapModal, setShowSwapModal] = useState(false);
+    const [showLiquidityModal, setShowLiquidityModal] = useState(false);
+    const [showBridgeModal, setShowBridgeModal] = useState(false);
+    const [showInfoModal, setShowInfoModal] = useState(false);
     const [showPasskeyGuideModal, setShowPasskeyGuideModal] = useState(false);
     const [showCubeGuideModal, setShowCubeGuideModal] = useState(false);
     const [showThemeGuideModal, setShowThemeGuideModal] = useState(false);
@@ -42,6 +60,12 @@ const Provider: FC<Props> = ({ children }) => {
             openStakingModal: () => setShowStakingModal(true),
             openLoginModal: () => setShowLoginModal(true),
             openParticlesGuideModal: () => setShowParticlesGuideModal(true),
+            openSendModal: () => setShowSendModal(true),
+            openReceiveModal: () => setShowReceiveModal(true),
+            openSwapModal: () => setShowSwapModal(true),
+            openLiquidityModal: () => setShowLiquidityModal(true),
+            openBridgeModal: () => setShowBridgeModal(true),
+            openInfoModal: () => setShowInfoModal(true),
         }}>
             <ThemeProvider>
                 <SorobanReactProvider>
@@ -49,6 +73,12 @@ const Provider: FC<Props> = ({ children }) => {
                     <AirdropModal isOpen={showAirdropModal} onClose={() => setShowAirdropModal(false)} />
                     <StakingModal isOpen={showStakingModal} onClose={() => setShowStakingModal(false)} />
                     <LoginModal isOpen={showLoginModal} onClose={() => setShowLoginModal(false)} />
+                    <SendModal isOpen={showSendModal} onClose={() => setShowSendModal(false)} />
+                    <ReceiveModal isOpen={showReceiveModal} onClose={() => setShowReceiveModal(false)} />
+                    <SwapModal isOpen={showSwapModal} onClose={() => setShowSwapModal(false)} />
+                    <LiquidityModal isOpen={showLiquidityModal} onClose={() => setShowLiquidityModal(false)} />
+                    <BridgeModal isOpen={showBridgeModal} onClose={() => setShowBridgeModal(false)} />
+                    <InfoModal isOpen={showInfoModal} onClose={() => setShowInfoModal(false)} />
                     <PasskeyGuideModal isOpen={showPasskeyGuideModal} onClose={() => setShowPasskeyGuideModal(false)} />
                     <CubeGuideModal isOpen={showCubeGuideModal} onClose={() => setShowCubeGuideModal(false)} />
                     <ThemeGuideModal isOpen={showThemeGuideModal} onClose={() => setShowThemeGuideModal(false)} />
