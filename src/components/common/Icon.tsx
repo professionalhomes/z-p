389bd2bddb501c8e54841ebb005107fb266e6325
professionalhomes <lednerdonny5@gmail.com>
2025-01-25 03:40:10 +0900

    feat: update-mouse-click-icon

--- src/components/common/Icon.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-import { Box, BoxProps } from "@chakra-ui/react";
-
-const Icon: FC<BoxProps> = (props) => {
-    return <Box {...props} />
-}
-
-export default Icon;
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,7 +1,1 @@
-export interface IconProps {
-    w?: string; h?: string;
-    color?: string;
-}
-
 export { default as MouseClickIcon } from "./MouseClickIcon";
-
