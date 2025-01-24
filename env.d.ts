8d83e1912c0ccf9201257e3dabaf3742335ce9ef
professionalhomes <lednerdonny5@gmail.com>
2025-01-25 03:46:02 +0900

    feat: add-airdrop-contract-id-to-env

--- env.d.ts
+++ env.d.ts
@@ -3,5 +3,6 @@ declare namespace NodeJS {
         NEXT_PUBLIC_STELLAR_RPC_URL: string;
         NEXT_PUBLIC_STELLAR_NETWORK_PASSPHRASE: string;
         NEXT_PUBLIC_FACTORY_CONTRACT_ID: string;
+        NEXT_PUBLIC_AIRDROP_CONTRACT_ID: string;
     }
 }
