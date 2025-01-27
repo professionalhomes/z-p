859a6d05117c3bd15a59bbc219cc211debaac277
professionalhomes <lednerdonny5@gmail.com>
2025-01-27 10:46:34 +0900

    feat: update-airdrop-modal

--- src/components/modals/AirdropModal.tsx
+++ src/components/modals/AirdropModal.tsx
@@ -21,30 +21,50 @@ const AirdropModal: FC<ModalProps> = ({ onClose, ...props }) => {
                 </Heading>
                 <Text textAlign='center'>
                     <Flex direction='column' gap={4}>
-                        <Button size='xl' onClick={() => {
-                            openPasskeyGuideModal?.();
-                            onClose?.();
-                        }}>
-                            Connect wallet
-                        </Button>
-                        <Button size='xl' onClick={() => {
-                            openCubeGuideModal?.();
-                            onClose?.();
-                        }}>
-                            Spin cube
-                        </Button>
-                        <Button size='xl' onClick={() => {
-                            openParticlesGuideModal?.();
-                            onClose?.();
-                        }}>
-                            Create particles
-                        </Button>
-                        <Button size='xl' onClick={() => {
-                            openThemeGuideModal?.();
-                            onClose?.();
-                        }}>
-                            Change theme
-                        </Button>
+                        <Flex direction='column' gap={2}>
+                            <Text textAlign='left'>
+                                Step 1). Use PasskeyID to Connect a Wallet
+                            </Text>
+                            <Button size='xl' onClick={() => {
+                                openPasskeyGuideModal?.();
+                                onClose?.();
+                            }}>
+                                Connect wallet
+                            </Button>
+                        </Flex>
+                        <Flex direction='column' gap={2}>
+                            <Text textAlign='left'>
+                                Step 2). Spin the Cube to get the first Airdrop
+                            </Text>
+                            <Button size='xl' onClick={() => {
+                                openCubeGuideModal?.();
+                                onClose?.();
+                            }}>
+                                Spin cube
+                            </Button>
+                        </Flex>
+                        <Flex direction='column' gap={2}>
+                            <Text textAlign='left'>
+                                Step 3). Create So Particles by clicking the screen
+                            </Text>
+                            <Button size='xl' onClick={() => {
+                                openParticlesGuideModal?.();
+                                onClose?.();
+                            }}>
+                                Create particles
+                            </Button>
+                        </Flex>
+                        <Flex direction='column' gap={2}>
+                            <Text textAlign='left'>
+                                Step 4). Change the Teal Moon theme to get your last Zi AirDrop
+                            </Text>
+                            <Button size='xl' onClick={() => {
+                                openThemeGuideModal?.();
+                                onClose?.();
+                            }}>
+                                Change theme
+                            </Button>
+                        </Flex>
                     </Flex>
                 </Text>
             </ModalContent>
