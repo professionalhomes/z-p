54be296830aa83bbca341f0c4bbfcbbc5e0700d0
professionalhomes <lednerdonny5@gmail.com>
2025-01-29 01:40:20 +0900

    feat: update-env-files

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,5 @@
 declare namespace NodeJS {
     interface ProcessEnv {
-        NEXT_PUBLIC_STELLAR_RPC_URL: string;
-        NEXT_PUBLIC_STELLAR_NETWORK_PASSPHRASE: string;
         NEXT_PUBLIC_FACTORY_CONTRACT_ID: string;
         NEXT_PUBLIC_AIRDROP_CONTRACT_ID: string;
     }
