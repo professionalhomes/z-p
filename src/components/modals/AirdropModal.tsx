742a65571b784ad9ce2beefddd89c8002b08ac63
professionalhomes <lednerdonny5@gmail.com>
2025-02-03 15:27:17 +0900

    feat: update-airdrop-modal

--- src/components/modals/AirdropModal.tsx
+++ src/components/modals/AirdropModal.tsx
@@ -1,6 +1,7 @@
 import { FC, useContext } from "react";
 
 import { Flex, Heading, Text } from "@chakra-ui/react";
+import { useSorobanReact } from "@soroban-react/core";
 
 import { AppContext } from "@/providers";
 
@@ -9,7 +10,8 @@ import { Modal, ModalCloseButton, ModalContent, ModalOverlay } from "../common";
 import { ModalProps } from "../common/Modal";
 
 const AirdropModal: FC<ModalProps> = ({ onClose, ...props }) => {
-    const { openPasskeyGuideModal, openCubeGuideModal, openParticlesGuideModal, openThemeGuideModal } = useContext(AppContext);
+    const { address } = useSorobanReact();
+    const { openPasskeyGuideModal, openCubeGuideModal, openParticlesGuideModal, openThemeGuideModal, setStartAnimation } = useContext(AppContext);
 
     return (
         <Modal onClose={onClose} {...props}>
@@ -21,50 +23,56 @@ const AirdropModal: FC<ModalProps> = ({ onClose, ...props }) => {
                 </Heading>
                 <Text textAlign='center'>
                     <Flex direction='column' gap={4}>
-                        <Flex direction='column' gap={2}>
-                            <Text textAlign='left'>
-                                Step 1). Use PasskeyID to Connect a Wallet
-                            </Text>
-                            <Button size='xl' onClick={() => {
-                                openPasskeyGuideModal?.();
-                                onClose?.();
-                            }}>
-                                Connect wallet
-                            </Button>
-                        </Flex>
-                        <Flex direction='column' gap={2}>
-                            <Text textAlign='left'>
-                                Step 2). Spin the Cube to get the first Airdrop
-                            </Text>
-                            <Button size='xl' onClick={() => {
-                                openCubeGuideModal?.();
-                                onClose?.();
-                            }}>
-                                Spin cube
-                            </Button>
-                        </Flex>
-                        <Flex direction='column' gap={2}>
-                            <Text textAlign='left'>
-                                Step 3). Create So Particles by clicking the screen
-                            </Text>
-                            <Button size='xl' onClick={() => {
-                                openParticlesGuideModal?.();
-                                onClose?.();
-                            }}>
-                                Create particles
-                            </Button>
-                        </Flex>
-                        <Flex direction='column' gap={2}>
-                            <Text textAlign='left'>
-                                Step 4). Change the Teal Moon theme to get your last Zi AirDrop
-                            </Text>
-                            <Button size='xl' onClick={() => {
-                                openThemeGuideModal?.();
-                                onClose?.();
-                            }}>
-                                Change theme
-                            </Button>
-                        </Flex>
+                        {!address ? (
+                            <Flex direction='column' gap={2}>
+                                <Text textAlign='left'>
+                                    Step 1). Use PasskeyID to Connect a Wallet
+                                </Text>
+                                <Button size='xl' onClick={() => {
+                                    openPasskeyGuideModal?.();
+                                    setStartAnimation?.(true);
+                                    onClose?.();
+                                }}>
+                                    Connect wallet
+                                </Button>
+                            </Flex>
+                        ) : (
+                            <>
+                                <Flex direction='column' gap={2}>
+                                    <Text textAlign='left'>
+                                        Step 2). Spin the Cube to get the first Airdrop
+                                    </Text>
+                                    <Button size='xl' onClick={() => {
+                                        openCubeGuideModal?.();
+                                        onClose?.();
+                                    }}>
+                                        Spin cube
+                                    </Button>
+                                </Flex>
+                                <Flex direction='column' gap={2}>
+                                    <Text textAlign='left'>
+                                        Step 3). Create So Particles by clicking the screen
+                                    </Text>
+                                    <Button size='xl' onClick={() => {
+                                        openParticlesGuideModal?.();
+                                        onClose?.();
+                                    }}>
+                                        Create particles
+                                    </Button>
+                                </Flex>
+                                <Flex direction='column' gap={2}>
+                                    <Text textAlign='left'>
+                                        Step 4). Change the Teal Moon theme to get your last Zi AirDrop
+                                    </Text>
+                                    <Button size='xl' onClick={() => {
+                                        openThemeGuideModal?.();
+                                        onClose?.();
+                                    }}>
+                                        Change theme
+                                    </Button>
+                                </Flex>
+                            </>
+                        )}
                     </Flex>
                 </Text>
             </ModalContent>
