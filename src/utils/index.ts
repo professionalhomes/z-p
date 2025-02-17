acbb86a2c913cf74bbe09b9d47c37c89d077d899
professionalhomes <lednerdonny5@gmail.com>
2025-02-17 11:52:31 +0900

    feat: add-fetchAssetImage-util-func

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
+import TOML from '@iarna/toml';
+import { Asset } from '@stellar-asset-lists/sdk';
+
 export * from './convert';
 
 export function truncateAddress(address: string): string {
@@ -8,4 +11,20 @@ export function truncateAddress(address: string): string {
     const start = address.slice(0, 4);
     const end = address.slice(-4);
     return `${start}...${end}`;
+}
+
+export async function fetchAssetImage(asset: Asset) {
+    try {
+        const response = await fetch(`https://${asset.domain}/.well-known/stellar.toml`);
+        const tomlData = await response.text();
+        const parsedToml = TOML.parse(tomlData) as any;
+
+        if (parsedToml.CURRENCIES) {
+            const currency = parsedToml.CURRENCIES.find((currency: any) => currency.code == asset.code);
+            return currency.image;
+        }
+    } catch (error) {
+        console.error('Failed to fetch or parse TOML for:', asset.code);
+    }
+    return null;
 }
