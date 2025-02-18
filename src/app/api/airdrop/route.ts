9fa3d82d5f68dc43deb943fa0cd7ae0f58b00aa8
professionalhomes <lednerdonny5@gmail.com>
2025-02-19 03:49:17 +0900

    feat: update-airdrop-api

--- src/app/api/airdrop/route.ts
+++ src/app/api/airdrop/route.ts
@@ -4,10 +4,11 @@ import { BASE_FEE, Contract, Keypair, TransactionBuilder, xdr } from '@stellar/s
 import { Server } from '@stellar/stellar-sdk/rpc';
 
 import { activeChain } from '@/lib/chain';
-import { accountToScVal } from '@/utils';
+import { accountToScVal, getErrorCode } from '@/utils';
 
-const airdropContractId = process.env.NEXT_PUBLIC_AIRDROP_CONTRACT_ID!;
 const airdropSourceSecret = process.env.AIRDROP_SOURCE_SECRET!;
+const airdropContractId = process.env.NEXT_PUBLIC_AIRDROP_CONTRACT_ID!;
+const ziContractId = process.env.NEXT_PUBLIC_ZI_CONTRACT_ID!;
 
 export async function POST(req: NextRequest) {
     try {
@@ -30,28 +31,36 @@ export async function POST(req: NextRequest) {
             }
         );
 
-        const tx = transaction.addOperation(
-            contract.call(
-                'distribute_tokens',
-                accountToScVal(address),
-                xdr.ScVal.scvU32(action)
-            )
-        ).setTimeout(30).build();
+        try {
+            const tx = transaction.addOperation(
+                contract.call(
+                    'distribute_tokens',
+                    accountToScVal(ziContractId),
+                    accountToScVal(sourceKeypair.publicKey()),
+                    accountToScVal(address),
+                    xdr.ScVal.scvU32(action)
+                )
+            ).setTimeout(30).build();
 
+            const preparedTx = await server.prepareTransaction(tx);
 
-        const preparedTx = await server.prepareTransaction(tx);
+            preparedTx.sign(sourceKeypair);
 
-        preparedTx.sign(sourceKeypair);
+            const result = await server.sendTransaction(preparedTx);
 
-        const result = await server.sendTransaction(preparedTx);
+            let response = await server.getTransaction(result.hash);
+            while (response.status === "NOT_FOUND") {
+                await new Promise(resolve => setTimeout(resolve, 2000));
+                response = await server.getTransaction(result.hash);
+            }
 
-        let response = await server.getTransaction(result.hash);
-        while (response.status === "NOT_FOUND") {
-            await new Promise(resolve => setTimeout(resolve, 2000));
-            response = await server.getTransaction(result.hash);
+            return NextResponse.json({ success: true, result });
+        } catch (err: any) {
+            const errorCode = getErrorCode(err.message);
+            if (errorCode == '1')
+                throw new Error("You've already received this type of airdrop.");
+            throw new Error('Unknown error occurred');
         }
-
-        return NextResponse.json({ success: true, result });
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
