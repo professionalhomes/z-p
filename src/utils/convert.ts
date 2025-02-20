6903be71236a21b7cbc2bbd0fd4101448e40c2a2
professionalhomes <lednerdonny5@gmail.com>
2025-02-20 13:18:31 +0900

    feat: add-scValToNumber-utils-func

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,5 +1,10 @@
-import { Address, xdr } from '@stellar/stellar-sdk';
+import { TxResponse } from '@soroban-react/contracts';
+import { Address, scValToBigInt, xdr } from '@stellar/stellar-sdk';
 
 export function accountToScVal(account: string): xdr.ScVal {
     return new Address(account).toScVal();
+}
+
+export function scValToNumber(scVal: xdr.ScVal | TxResponse) {
+    return Number(scValToBigInt(scVal as any));
 }
