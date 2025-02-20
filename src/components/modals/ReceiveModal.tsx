e63e1de12125a5d005f81374fd2a3b27d4a5865a
professionalhomes <lednerdonny5@gmail.com>
2025-02-20 12:33:35 +0900

    feat: impl-receive-modal

--- src/components/modals/ReceiveModal.tsx
+++ src/components/modals/ReceiveModal.tsx
@@ -1,22 +1,42 @@
 import { FC } from "react";
 
-import { Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, QrCode, Text } from "@chakra-ui/react";
+import { useSorobanReact } from "@soroban-react/core";
+
+import { truncateAddress } from "@/utils";
 
 import { Modal, ModalCloseButton, ModalContent, ModalOverlay } from "../common";
 import { ModalProps } from "../common/Modal";
+import { ClipboardIconButton, ClipboardRoot } from "../ui/clipboard";
 
 const ReceiveModal: FC<ModalProps> = (props) => {
+    const { address } = useSorobanReact();
+
     return (
         <Modal {...props}>
             <ModalOverlay />
-            <ModalContent left={{ base: '50%', lg: '75%' }} p={8} w='full' maxW={{ base: '320px', lg: '420px' }} direction='column' gap={4}>
+            <ModalContent left={{ base: '50%', lg: '75%' }} p={8} w='full' maxW="280px" direction='column' gap={4}>
                 <ModalCloseButton />
                 <Heading as="h2" textAlign="center" size="lg">
                     RECEIVE
                 </Heading>
-                <Text textAlign='center'>
-                    Coming soon...
-                </Text>
+                <Flex direction="column" align="center" gap={4}>
+                    <Box p={1} bg="white">
+                        <QrCode.Root value={address} color="black">
+                            <QrCode.Frame>
+                                <QrCode.Pattern />
+                            </QrCode.Frame>
+                        </QrCode.Root>
+                    </Box>
+                    <Flex align="center" gap={2}>
+                        <Text fontSize="small">
+                            {truncateAddress(address)}
+                        </Text>
+                        <ClipboardRoot value={address}>
+                            <ClipboardIconButton />
+                        </ClipboardRoot>
+                    </Flex>
+                </Flex>
             </ModalContent>
         </Modal>
     )
