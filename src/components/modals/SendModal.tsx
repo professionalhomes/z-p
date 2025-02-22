341967328b8f7cb51a8a7619492f80816e19bc57
professionalhomes <lednerdonny5@gmail.com>
2025-02-22 17:59:39 +0900

    feat: impl-send-asset-func

--- src/components/modals/SendModal.tsx
+++ src/components/modals/SendModal.tsx
@@ -1,11 +1,47 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
-import { Heading, Text } from "@chakra-ui/react";
+import { Flex, Heading, Image, Input, Spinner, Text } from "@chakra-ui/react";
+import { useSorobanReact } from "@soroban-react/core";
+import { Asset } from "@stellar-asset-lists/sdk";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+import useAssets from "@/hooks/useAssets";
+import { sendAsset } from "@/services/contract";
+
+import Button from "../Button";
 import { Modal, ModalCloseButton, ModalContent, ModalOverlay } from "../common";
 import { ModalProps } from "../common/Modal";
+import { toaster } from "../ui/toaster";
 
 const SendModal: FC<ModalProps> = (props) => {
+    const sorobanContext = useSorobanReact();
+    const queryClient = useQueryClient();
+    const { assets } = useAssets();
+    const [asset, setAsset] = useState<Asset>();
+    const [recipient, setRecipient] = useState("");
+    const [amount, setAmount] = useState("");
+
+    const { mutate, isPending } = useMutation({
+        mutationFn: () => {
+            if (!asset) {
+                throw new Error('Please select asset to send.');
+            }
+            return sendAsset(sorobanContext, asset, recipient, Number(amount));
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['getAssetBalance', asset?.contract] }),
+                toaster.create({
+                    title: `You've sent asset successfully.`,
+                    type: 'success',
+                });
+        }
+    });
+
+    const handleSend = () => {
+        if (!asset) return;
+        mutate();
+    }
+
     return (
         <Modal {...props}>
             <ModalOverlay />
@@ -14,9 +50,85 @@ const SendModal: FC<ModalProps> = (props) => {
                 <Heading as="h2" textAlign="center" size="lg">
                     SEND
                 </Heading>
-                <Text textAlign='center'>
-                    Coming soon...
-                </Text>
+                {asset ? (
+                    <Flex direction="column" gap={2}>
+                        <Flex direction="column" gap={1}>
+                            <Text pl={2}>
+                                Recipient stellar address
+                            </Text>
+                            <Input
+                                p='1rem'
+                                bg='rgba(255, 255, 255, 0.15)'
+                                border='none'
+                                rounded='full'
+                                shadow='0 8px 32px 0 rgba(31, 38, 135, 0.37)'
+                                onChange={(e) => setRecipient(e.target.value)}
+                            />
+                        </Flex>
+                        <Flex direction="column" gap={1}>
+                            <Text pl={2}>
+                                Amount
+                            </Text>
+                            <Input
+                                p='1rem'
+                                bg='rgba(255, 255, 255, 0.15)'
+                                border='none'
+                                rounded='full'
+                                shadow='0 8px 32px 0 rgba(31, 38, 135, 0.37)'
+                                onChange={(e) => setAmount(e.target.value)}
+                            />
+                        </Flex>
+                        <Flex justify="right">
+                            <Button onClick={handleSend}>
+                                {isPending && <Spinner size="sm" />}
+                                Send
+                            </Button>
+                        </Flex>
+                    </Flex>
+                ) : (
+                    <Flex
+                        maxH='480px'
+                        direction='column'
+                        gap={1}
+                        overflowY='auto'
+                    >
+                        {assets.filter(asset => asset.balance && asset.balance > 0).map((asset, index) => (
+                            <Flex
+                                key={index}
+                                p={2}
+                                gap={2}
+                                rounded="md"
+                                cursor="pointer"
+                                _hover={{
+                                    bg: "rgba(255, 255, 255, 0.15)",
+                                }}
+                                onClick={() => setAsset(asset)}
+                            >
+                                <Flex flex='1 1 0' gap={2}>
+                                    <Image flex='none' w={10} h={10} alt={asset.code} src={asset.icon} />
+                                    <Flex direction='column' justify='space-around'>
+                                        <Text maxW='120px' fontSize='small' truncate>
+                                            {asset.name}
+                                        </Text>
+                                        <Text fontSize='x-small'>
+                                            {asset.code} ({asset.domain})
+                                        </Text>
+                                    </Flex>
+                                </Flex>
+                                <Flex direction='column' justify='space-around'>
+                                    {asset.balance != undefined ? (
+                                        <Text>
+                                            {asset.balance}
+                                        </Text>
+                                    ) : (
+                                        <Spinner size='sm' />
+                                    )}
+                                </Flex>
+                            </Flex>
+                        ))}
+                    </Flex>
+                )}
+
             </ModalContent>
         </Modal>
     )
--- src/services/contract.ts
+++ src/services/contract.ts
@@ -1,34 +1,30 @@
-import { accountToScVal, scValToNumber } from "@/utils";
 import { contractInvoke } from "@soroban-react/contracts";
 import { SorobanContextType } from "@soroban-react/core";
 import { Asset } from "@stellar-asset-lists/sdk";
+import { nativeToScVal } from "@stellar/stellar-sdk";
+
+import { accountToScVal, scValToNumber } from "@/utils";
 
 const airdropContractId = process.env.NEXT_PUBLIC_AIRDROP_CONTRACT_ID!;
 
 export async function tokenBalance(
   sorobanContext: SorobanContextType,
   tokenAddress: string,
-  userAddress: string,
 ) {
-  const user = accountToScVal(userAddress);
+  const { address } = sorobanContext;
 
-  if (!tokenAddress) return 0;
-
-  try {
-    const response = await contractInvoke({
-      contractAddress: tokenAddress,
-      method: 'balance',
-      args: [user],
-      sorobanContext,
-    });
-
-    const decimals = await tokenDecimals(sorobanContext, tokenAddress);
-
-    return scValToNumber(response as any) / Math.pow(10, decimals);
-  } catch (err) {
-    console.error(err);
-    return 0;
+  if (!address) {
+    throw new Error("Wallet is not connected yet.");
   }
+
+  const response = await contractInvoke({
+    contractAddress: tokenAddress,
+    method: 'balance',
+    args: [accountToScVal(address)],
+    sorobanContext,
+  });
+
+  return scValToNumber(response as any);
 }
 
 export async function tokenDecimals(sorobanContext: SorobanContextType, tokenAddress: string) {
@@ -48,14 +44,34 @@ export const getAirdropStatus = async (sorobanContext: SorobanContextType, addre
     args: [accountToScVal(address)],
     sorobanContext,
   });
+
   return scValToNumber(response as any);
 }
 
-export const sendAsset = (
+export const sendAsset = async (
   sorobanContext: SorobanContextType,
   asset: Asset,
   recipient: string,
-  amount: Number
+  amount: number
 ) => {
+  const { address } = sorobanContext;
 
+  if (!address) {
+    throw new Error("Wallet is not connected yet.");
+  }
+
+  const response = await contractInvoke({
+    contractAddress: asset.contract,
+    method: 'transfer',
+    args: [
+      accountToScVal(address),
+      accountToScVal(recipient),
+      nativeToScVal(amount * Math.pow(10, asset.decimals), { type: 'i128' })
+    ],
+    sorobanContext,
+    signAndSend: true,
+    reconnectAfterTx: false,
+  });
+
+  return response;
 }
