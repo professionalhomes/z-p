d9403b0711c523e85d20f68f6792b7e8064ac9a3
professionalhomes <lednerdonny5@gmail.com>
2025-02-25 21:18:33 +0900

    chore: update-passkey-server

--- src/lib/passkeyServer.ts
+++ src/lib/passkeyServer.ts
@@ -1,13 +1,19 @@
 import { PasskeyServer } from 'passkey-kit';
 
-const { LAUNCHTUBE_JWT, MERCURY_KEY } = process.env;
-const { NEXT_PUBLIC_PROJECT_NAME, NEXT_PUBLIC_LAUNCHTUBE_URL, NEXT_PUBLIC_MERCURY_URL, NEXT_PUBLIC_STELLAR_RPC_URL } = process.env;
+import { activeChain } from './chain';
+
+const rpcUrl = activeChain.sorobanRpcUrl!;
+const launchtubeUrl = process.env.NEXT_PUBLIC_LAUNCHTUBE_URL!;
+const launchtubeJwt = process.env.LAUNCHTUBE_JWT!;
+const mercuryProjectName = process.env.NEXT_PUBLIC_PROJECT_NAME!;
+const mercuryUrl = process.env.NEXT_PUBLIC_MERCURY_URL!;
+const mercuryKey = process.env.MERCURY_KEY!;
 
 export const server = new PasskeyServer({
-    rpcUrl: NEXT_PUBLIC_STELLAR_RPC_URL,
-    launchtubeUrl: NEXT_PUBLIC_LAUNCHTUBE_URL,
-    launchtubeJwt: LAUNCHTUBE_JWT,
-    mercuryProjectName: NEXT_PUBLIC_PROJECT_NAME,
-    mercuryUrl: NEXT_PUBLIC_MERCURY_URL,
-    mercuryKey: MERCURY_KEY,
+    rpcUrl,
+    launchtubeUrl,
+    launchtubeJwt,
+    mercuryProjectName,
+    mercuryUrl,
+    mercuryKey,
 });
