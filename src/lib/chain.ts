9515674be68afb8930e2d31dc6ee188e390874ca
professionalhomes <lednerdonny5@gmail.com>
2025-02-25 21:17:50 +0900

    chore: add-native-token-address-to-chain

--- src/lib/chain.ts
+++ src/lib/chain.ts
@@ -1,5 +1,8 @@
 import { mainnet, standalone, testnet } from '@soroban-react/chains';
 
+import { nativeTokens } from '@/constants';
+
 export const chains = process.env.NODE_ENV === 'production' ? [testnet, mainnet] : [standalone, testnet, mainnet];
 export const activeChainName = process.env.PUBLIC_STELLAR_NETWORK || 'testnet';
 export const activeChain = chains.find((chain) => chain.id === activeChainName) || testnet;
+export const nativeTokenAddress = nativeTokens.find((nativeToken) => nativeToken.network === activeChainName)!.address;
