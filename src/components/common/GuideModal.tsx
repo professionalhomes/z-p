d7b60e4d1029a99fc8cb84dfa8a51fcd8ffe4e71
professionalhomes <lednerdonny5@gmail.com>
2025-03-07 10:05:44 +0900

    feat: show-confetti-on-success

--- src/components/common/GuideModal.tsx
+++ src/components/common/GuideModal.tsx
@@ -1,54 +1,65 @@
 import { FC, ReactNode, useState } from "react";
 
-import { Flex, Spinner, Text } from "@chakra-ui/react";
+import { Flex, FlexProps, Text } from "@chakra-ui/react";
 
-import useAirdrop, { Action } from "@/hooks/useAirdrop";
+import { Action } from "@/hooks/useAirdrop";
 
 import { Modal, ModalCloseButton, ModalContent, ModalOverlay } from ".";
-import Button from "../Button";
+import GetAirdropButton from "../modals/guides/GetAirdropButton";
+import { GradientCanvas } from "./GradientCanvas";
 import { ModalProps } from "./Modal";
 
 interface Props extends ModalProps {
-    title: string;
-    description: string;
-    congratulation?: ReactNode;
-    action?: Action;
-    showButton?: boolean;
+  title: string;
+  description: string;
+  congratulation?: ReactNode;
+  action?: Action;
+  showButton?: boolean;
+  showGradientBackground?: boolean;
+  _modalContent?: FlexProps;
 }
 
-const GuideModal: FC<Props> = ({ title, description, congratulation, children, action, showButton, ...props }) => {
-    const { getAirdrop, isGettingAirdrop } = useAirdrop();
-
-    const [isCompleted, setIsCompleted] = useState(false);
-
-    return (
-        <Modal {...props}>
-            <ModalOverlay />
-            <ModalContent w='360px' p={4} direction='column' gap={2}>
-                <ModalCloseButton />
-                <Flex direction='column' gap={1}>
-                    <Text fontSize='xl'>{title}</Text>
-                    <Text fontSize='sm'>{description}</Text>
-                </Flex>
-                {children}
-                {(!isCompleted && showButton) && (
-                    <Button
-                        gap={1}
-                        disabled={isGettingAirdrop}
-                        onClick={async () => {
-                            if (action) {
-                                await getAirdrop(action);
-                                setIsCompleted(true);
-                            }
-                        }}>
-                        {isGettingAirdrop && <Spinner size='sm' />}
-                        Get airdrop
-                    </Button>
-                )}
-                {isCompleted && congratulation}
-            </ModalContent>
-        </Modal>
-    )
-}
+const GuideModal: FC<Props> = ({
+  title,
+  description,
+  congratulation,
+  children,
+  action,
+  showButton,
+  showGradientBackground,
+  _modalContent,
+  ...props
+}) => {
+  return (
+    <Modal {...props}>
+      <ModalOverlay />
+      <ModalContent
+        w="360px"
+        minH={showGradientBackground ? "360px" : undefined}
+        {..._modalContent}
+      >
+        {showGradientBackground && (
+          <GradientCanvas
+            position="absolute"
+            w="full"
+            h="full"
+            zIndex={-5}
+            rounded="16px"
+            overflow="hidden"
+          />
+        )}
+        <Flex w="full" p={4} direction="column" gap={2} overflow="hidden">
+          <ModalCloseButton />
+          <Flex direction="column" gap={1}>
+            <Text fontSize="xl">{title}</Text>
+            <Text fontSize="sm">{description}</Text>
+          </Flex>
+          {children}
+          {showButton && <GetAirdropButton action={action} />}
+        </Flex>
+      </ModalContent>
+    </Modal>
+  );
+};
 
 export default GuideModal;
