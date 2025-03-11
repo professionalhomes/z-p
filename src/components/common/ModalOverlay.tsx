5806532cac7604409f54a86b7d0fbdff647a66ed
professionalhomes <lednerdonny5@gmail.com>
2025-03-11 16:48:53 +0900

    feat: apply-transation-to-modal-overlay

--- src/components/common/ModalOverlay.tsx
+++ src/components/common/ModalOverlay.tsx
@@ -1,16 +1,23 @@
 import { Box, BoxProps } from "@chakra-ui/react";
-import { FC, useContext } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import { ModalContext } from "./Modal";
 
 const ModalOverlay: FC<BoxProps> = ({ ...props }) => {
     const { isOpen, onClose } = useContext(ModalContext);
+    const [opacity, setOpacity] = useState(0);
+
+    useEffect(() => {
+        setOpacity(isOpen ? 0.5 : 0);
+    }, [isOpen]);
 
     return <Box
         position='fixed'
         inset={0}
         zIndex={1010}
         display={isOpen ? 'flex' : 'none'}
-        bg='#0004'
+        bg='black'
+        opacity={opacity}
+        transition='opacity'
         onClick={onClose}
         {...props}
     />
