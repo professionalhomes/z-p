6015d44b8ca52b1cc48b9342d78a74fd56f3dcb6
professionalhomes <lednerdonny5@gmail.com>
2025-04-01 02:12:14 +0900

    fix: get signers api cache

--- src/app/api/signer/[contract_id]/route.ts
+++ src/app/api/signer/[contract_id]/route.ts
@@ -1,16 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { server } from "@/lib/passkeyServer";
+const mercuryProjectName = process.env.MERCURY_PROJECT_NAME!;
+const mercuryUrl = process.env.MERCURY_URL!;
+const mercuryJwt = process.env.MERCURY_JWT!;
 
 export async function GET(
   _: NextRequest,
   { params: { contract_id } }: { params: { contract_id: string } }
 ) {
   try {
-    const signers = await server.getSigners(contract_id);
-    const response = NextResponse.json(signers);
-    response.headers.set('Cache-Control', 'no-store');
-    return response;
+    const signers = await fetch(`${mercuryUrl}/zephyr/execute`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${mercuryJwt}`
+      },
+      body: JSON.stringify({
+        project_name: mercuryProjectName,
+        mode: {
+          Function: {
+            fname: "get_signers_by_address",
+            arguments: JSON.stringify({
+              address: contract_id,
+            }),
+          },
+        },
+      }),
+      cache: "no-store"
+    }).then(async (res) => {
+      if (res.ok) return res.json();
+      throw await res.json();
+    });
+    return NextResponse.json(signers);
   } catch (error) {
     return NextResponse.json(error, { status: 500 });
   }
