b556b5dfb61d1524004aedc7e4ec8423e77a1e52
professionalhomes <lednerdonny5@gmail.com>
2025-04-12 02:16:31 +0900

    fix: remove contract

--- contracts/zafegard-policy/src/types.rs
+++
@@ -1,9 +0,0 @@
-use soroban_sdk::{contracttype, Address, Bytes, BytesN};
-
-#[contracttype]
-#[derive(Clone, Debug, PartialEq)]
-pub enum SignerKey {
-    Policy(Address),
-    Ed25519(BytesN<32>),
-    Secp256r1(Bytes),
-}
