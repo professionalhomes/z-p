712dcddef7f9f15c81ae16bdd585eabff80873c2
professionalhomes <lednerdonny5@gmail.com>
2025-04-12 01:30:47 +0900

    feat: split contract to be & fe

--- src/app/api/airdrop/route.ts
+++ src/app/api/airdrop/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { xdr } from "@stellar/stellar-sdk";
 
-import { contractInvoke } from "@/lib/contract";
+import { contractInvoke } from "@/lib/contract-be";
 import { accountToScVal } from "@/utils";
 
 const airdropContractId = process.env.NEXT_PUBLIC_AIRDROP_CONTRACT_ID!;
