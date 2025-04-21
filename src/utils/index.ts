e17f249c38b94b258e0b8bacfbe625bd6ce74870
professionalhomes <lednerdonny5@gmail.com>
2025-04-22 02:16:17 +0900

    chore: remove fetchAssetImage utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,3 @@
-import TOML from "@iarna/toml";
-import { Asset } from "@stellar-asset-lists/sdk";
-
 export * from "./convert";
 
 export function truncateAddress(address?: string | null): string {
@@ -11,31 +8,17 @@ export function truncateAddress(address?: string | null): string {
   return `${start}...${end}`;
 }
 
-export const formatNumber = (value: number | string, fractionDigits: number = 2) => {
-  if (typeof value == 'number' && !isNaN(value))
-    return value.toFixed(fractionDigits).replace(/\.+[0-9]*$/, (substring) => substring.replace(/\.*0*$/, ''));
-  if (typeof value == 'string')
-    return value.replace(/\.+[0-9]*$/, (substring) => substring.slice(0, fractionDigits + 1));
-  return '0';
-}
-
-export async function fetchAssetImage(asset: Asset) {
-  try {
-    const response = await fetch(
-      `https://${asset.domain}/.well-known/stellar.toml`
+export const formatNumber = (
+  value: number | string,
+  fractionDigits: number = 2
+) => {
+  if (typeof value == "number" && !isNaN(value))
+    return value
+      .toFixed(fractionDigits)
+      .replace(/\.+[0-9]*$/, (substring) => substring.replace(/\.*0*$/, ""));
+  if (typeof value == "string")
+    return value.replace(/\.+[0-9]*$/, (substring) =>
+      substring.slice(0, fractionDigits + 1)
     );
-    const tomlData = await response.text();
-    const parsedToml = TOML.parse(tomlData) as any;
-
-    if (parsedToml.CURRENCIES) {
-      const currency = parsedToml.CURRENCIES.find(
-        (currency: any) => currency.code == asset.code
-      );
-      return currency.image;
-    }
-  } catch (err: any) {
-    console.error(err.message);
-  }
-
-  return null;
-}
+  return "0";
+};
