7a9d11c9505950df19e51258bc48ee3c8b62b1ed
professionalhomes <lednerdonny5@gmail.com>
2025-06-25 15:46:39 +0900

    feat: send reward when complete game

--- supabase/functions/score/index.ts
+++ supabase/functions/score/index.ts
@@ -1,15 +1,21 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { createClient } from "jsr:@supabase/supabase-js@2";
+import { nativeToScVal } from "npm:@stellar/stellar-sdk";
 import {
   BadRequestException,
   handleException,
   MethodNotAllowedException
 } from "../exceptions.ts";
+import { contractInvoke } from "../contract.ts";
 
 const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
 const supabasekey = Deno.env.get("SUPABASE_ANON_KEY")!;
 
 const supabase = createClient(supabaseUrl, supabasekey);
+
+const funderPublicKey = Deno.env.get("FUNDER_PUBLIC_KEY")!;
+const funderSecretKey = Deno.env.get("FUNDER_SECRET_KEY")!;
+const zionTokenAddress = Deno.env.get("ZION_TOKEN_ADDRESS")!;
 
 Deno.serve((req) =>
   handleException(async () => {
@@ -39,6 +45,21 @@ const createScore = async (createScoreDto: any) => {
     .single();
   if (scoreError) throw new Error(scoreError.message);
   if (!data) throw new Error("Failed to create score");
+
+  const result = await contractInvoke({
+    contractAddress: zionTokenAddress,
+    method: "transfer",
+    secretKey: funderSecretKey,
+    args: [
+      nativeToScVal(funderPublicKey, { type: "address" }),
+      nativeToScVal(data.publicKey, { type: "address" }),
+      nativeToScVal(data.score * 100, { type: "i128" }),
+    ],
+  });
+
+  if (result.status != "SUCCESS") {
+    throw new Error("Failed to create score");
+  }
 
   return data;
 };
